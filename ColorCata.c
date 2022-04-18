b5da3a44a1d317edc68598cc1a2aa91ac2c5e946
MohammadAbdullahkhan1 <100206902+MohammadAbdullahkhan1@users.noreply.github.com>
2022-04-18 14:01:43 +0530

    Update ColorCata.c

--- ColorCata.c
+++ ColorCata.c
@@ -2,24 +2,24 @@
 #include <assert.h>
 #include "col.h"
 
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
-const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
-int numberOfMajorColors =  sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-int pairNumber = 1;
+void PrintColorRefrence(int * MajorColorIdx)
+{
+   int lMajorColorIdx;
+   int lMinorColorIdx =0;   
+   lMajorColorIdx = *MajorColorIdx;
+   for( lMinorColorIdx = 0; lMinorColorIdx < numberOfMinorColors; lMinorColorIdx++ )
+   {
+       printf( "%d\t             %s\t            %s\t\n",pairNumber, MajorColorNames[lMajorColorIdx], MinorColorNames[lMinorColorIdx]);
+       pairNumber++;
+   }
+}
 
 void ColorCatalogue()
 {
-  int lMajorColorIdx;
-  int lMinorColorIdx =0;
+  int lMajorColorIdx;  
   printf("PairNumber         MajorColor        MinorColor \n");  
   for( lMajorColorIdx = 0; lMajorColorIdx < numberOfMajorColors; lMajorColorIdx++ )
   {
-        for( lMinorColorIdx = 0; lMinorColorIdx < numberOfMinorColors; lMinorColorIdx++ )
-         {
-             printf( "%d\t             %s\t            %s\t\n",pairNumber, MajorColorNames[lMajorColorIdx], MinorColorNames[lMinorColorIdx]);
-             pairNumber++;
-         }
+     PrintColorRefrence(&lMajorColorIdx);
   }
 }
